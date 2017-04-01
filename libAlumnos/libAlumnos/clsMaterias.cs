57dc0cab0dda1ce7ade9e71cc96468e0ee63ea4a
MiguelParamo <mparamo94@hotmail.com>
2017-04-01 09:42:51 -0600

    Comentarios
    Prueba de Comentarios

--- libAlumnos/libAlumnos/clsMaterias.cs
+++ libAlumnos/libAlumnos/clsMaterias.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//prueba de Comentarios
 namespace libAlumnos
 {
     public class clsMaterias
